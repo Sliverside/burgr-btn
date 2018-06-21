ee018f408806bbd48c3b2bdd16e59f595878784e
lilian <sliverside95@gmail.com>
2018-06-21 03:53:48 +0200

    gulp workflow improuvment

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const sass = require("gulp-sass")
 const postcss = require("gulp-postcss")
 const autoprefixer = require("autoprefixer")
 const cssnano = require("cssnano")
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
+const logger = require('gulp-logger')
 
 var ignoredFolders = "node_modules, .git"
 var autoprefixerVersion = "last 2 versions"
@@ -13,8 +16,24 @@ gulp.task("watch", () => watchsass(
     ["./**/*.scss", "!./{" + ignoredFolders + "}/**/*"],
     {base: "./"}
   )
+  .pipe(plumber({ errorHandler: function(err) {
+    notify.onError({
+      title: "Error in " + err.plugin,
+      text: 'error : `' + err.messageOriginal + '` on line ' + err.line + ' of ' + err.relativePath + '\n look at the console for more details'
+    })(Error('\n' + err.messageFormatted));
+  }}))
   .pipe(sourcemaps.init())
   .pipe(sass())
+  .pipe(logger({
+        before: 'Compiling scss file(s)...',
+        extname: '.scss'
+    }))
   .pipe(postcss([ autoprefixer({browsers: [autoprefixerVersion]}), cssnano() ]))
+  .pipe(notify({title: 'SCSS compilling complete !', text: 'SCSS file(s) has been compiled with success !', onLast: false}))
   .pipe(sourcemaps.write("./"))
-  .pipe(gulp.dest("./")));
+  .pipe(gulp.dest("./"))
+  .pipe(logger({
+        before: 'Creating files...',
+        after: 'Files has been created with success !'
+    }))
+);
