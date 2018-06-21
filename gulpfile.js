6ed95ae16e3d2db44939746844b7b27a70b2c603
lilian <sliverside95@gmail.com>
2018-06-22 01:27:48 +0200

    improuvment of BPS mixin

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,15 @@ const sourcemaps = require("gulp-sourcemaps")
 const watchsass = require("gulp-watch-sass")
 const sass = require("gulp-sass")
 const postcss = require("gulp-postcss")
-const autoprefixer = require("autoprefixer")
-const cssnano = require("cssnano")
 const mqpacker = require("css-mqpacker")
+const autoprefixer = require("autoprefixer")
+const cleancss = require("gulp-clean-css")
 const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
 const logger = require('gulp-logger')
 
 var ignoredFolders = "node_modules, .git"
 var autoprefixerVersion = "last 2 versions"
-var cssnanoConfig = {preset: 'default'}
 
 gulp.task("watch", () => watchsass(
     ["./**/*.scss", "!./{" + ignoredFolders + "}/**/*"],
@@ -30,7 +29,10 @@ gulp.task("watch", () => watchsass(
         before: 'Compiling scss file(s)...',
         extname: '.scss'
     }))
-  .pipe(postcss([ autoprefixer({browsers: [autoprefixerVersion]}), mqpacker(), cssnano(cssnanoConfig) ]))
+  .pipe(postcss([ mqpacker(), autoprefixer({browsers: [autoprefixerVersion]}) ]))
+  .pipe(cleancss({debug: true, level: 2}, (details) => {
+    console.log(`[??:??:??] 'cleancss' minify file from ${details.stats.originalSize}mb to ${details.stats.minifiedSize}mb`)
+  }))
   .pipe(notify({title: 'SCSS compilling complete !', text: 'SCSS file(s) has been compiled with success !', onLast: false}))
   .pipe(sourcemaps.write("./"))
   .pipe(gulp.dest("./"))
